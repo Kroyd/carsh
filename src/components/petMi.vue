9a1307e33f3f371f320fced490fc21814fb2c923
Kroyd <kroydjaykven@gmail.com>
2023-07-04 15:58:14 +0500

    1

---
+++ src/components/petMi.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pet">
+    <h3>{{ pet.name }}</h3>
+    <MyButton label="Save"/>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pet: Object,
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/petsMi.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pets">
+        <div v-for="pet in pets" :key="pet.id">
+            <pet-mi :pet="pet"></pet-mi>
+        </div>
+    </div>
+</template>
+
+<script>
+import petMi from './petMi.vue';
+
+export default {
+    components: { petMi },
+    props: {
+        pets: Array,
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
